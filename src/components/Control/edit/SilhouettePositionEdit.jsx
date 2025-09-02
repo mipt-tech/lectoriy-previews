02391cdaaec4b55bd1eeee778e173c65786b6fd8
renatxat <khatymov.rr@phystech.edu>
2025-09-02 21:25:51 +0300

    add hints

--- src/components/Control/edit/SilhouettePositionEdit.jsx
+++ src/components/Control/edit/SilhouettePositionEdit.jsx
@@ -4,6 +4,10 @@ import { setAdditionalX } from '../../../store/actions'
 import Slider from '@material-ui/core/Slider'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
+import Tooltip from '@material-ui/core/Tooltip'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
+
+import styles from '../Control.css'
 
 const SilhouettePositionEdit = () => {
   const x = useSelector(state => state.additional_x)
@@ -16,12 +20,21 @@ const SilhouettePositionEdit = () => {
   return (
     <Grid container>
       <Grid item xs={12}>
-        <Typography variant="caption" color="textSecondary">
-          Горизонтальное положение
-        </Typography>
+        <Grid container spacing={1} alignItems="center">
+          <Grid item>
+            <Typography variant="caption" color="textSecondary">
+              Горизонтальное положение
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Tooltip title="Оставьте не менее половины обложки для темы занятия">
+              <InfoOutlinedIcon className={styles.infoIcon} />
+            </Tooltip>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item xs={12}>
-        <Slider value={x + 1} onChange={onChange} min={0} step={0.01} max={2} track={false} />
+        <Slider value={x + 1} onChange={onChange} min={0} step={0.01} max={2} track="normal" />
       </Grid>
     </Grid>
   )
--- src/components/Control/edit/SilhouetteScaleEdit.jsx
+++ src/components/Control/edit/SilhouetteScaleEdit.jsx
@@ -4,6 +4,9 @@ import { setAdditionalScale } from '../../../store/actions'
 import Slider from '@material-ui/core/Slider'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
+
+import styles from '../Control.css'
 
 const SilhouetteScaleEdit = () => {
   const scale = useSelector(state => state.additional_scale)
@@ -13,15 +16,23 @@ const SilhouetteScaleEdit = () => {
     dispatch(setAdditionalScale(value))
   }
 
+  // Добавляем невидимую иконку, чтобы с silhouettePositionEdit быть на одном уровне
   return (
     <Grid container>
       <Grid item xs={12}>
-        <Typography variant="caption" color="textSecondary">
-          Размер
-        </Typography>
+        <Grid container spacing={1}>
+          <Grid item>
+            <Typography variant="caption" color="textSecondary">
+              Размер
+            </Typography>
+          </Grid>
+          <Grid item>
+            <InfoOutlinedIcon className={styles.infoIcon} style={{ visibility: 'hidden' }} />
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item xs={12}>
-        <Slider value={scale} onChange={onChange} min={0.5} step={0.01} max={2} />
+        <Slider value={scale} onChange={onChange} min={0.5} step={0.01} max={2} track="normal" />
       </Grid>
     </Grid>
   )
